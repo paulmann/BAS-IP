ad9d2706066f0e45d71d24d505da4e510071df61
Mikhail Deynekin <mid1977@gmail.com>
2025-03-28 20:57:03 +0300

    Update bas-ip.php

--- bas-ip.php
+++ bas-ip.php
@@ -27,9 +27,7 @@ if (!in_array($_SERVER['REMOTE_ADDR'], $config['allowedIps'], true)) {
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 	<link rel="stylesheet" href="css/main.css" />
- 	<link rel="stylesheet" href="/css/styles.css" />
 	<link rel="icon" href="/favicon.svg" type="image/svg">
-	<noscript><link rel="stylesheet" href="css/noscript.css" /></noscript>
 </head>
 <body class="is-preload">
 	<div id="confirmationModal" class="confirmation-modal">
